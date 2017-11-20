bcedb65e7127c62e3d4ce566687ce5f078a40387
Hanna Bennett <bennetthanna22@gmail.com>
2017-11-20 15:37:05 -0800

    Creat fragment and layout

---
+++ app/src/main/java/com/example/hannabennett/namemanglersrevenge/MangledNameFragment.java
@@ -0,0 +1,11 @@
+package com.example.hannabennett.namemanglersrevenge;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by HannaBennett on 11/20/17.
+ */
+
+public class MangledNameFragment extends Fragment {
+
+}
