8d12bdc174797b10f0ce86f107617d288f65794e
Hanna Bennett <bennetthanna22@gmail.com>
2017-11-21 23:38:19 -0800

    WIP trying to set on click listeners to reset and remangle button

--- app/src/main/java/com/example/hannabennett/namemanglersrevenge/MangledNameViewModel.java
+++ app/src/main/java/com/example/hannabennett/namemanglersrevenge/MangledNameViewModel.java
@@ -1,5 +1,7 @@
 package com.example.hannabennett.namemanglersrevenge;
 
+import android.app.Activity;
+
 /**
  * Created by HannaBennett on 11/21/17.
  */
@@ -14,4 +16,13 @@ public class MangledNameViewModel {
     public String getMangledName() {
         return mMangledName.getMangledName();
     }
+
+//    public void resetButtonClicked() {
+//        Activity activity = (Activity)getContext();
+//        activity.finish();
+//    }
+
+//    public void remangleButtonClicked() {
+//        mMangledName = new MangledName(inputName, randomLastName);
+//    }
 }
